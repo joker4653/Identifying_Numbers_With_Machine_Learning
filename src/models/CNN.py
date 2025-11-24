channel mismatch, fixed to a simpler example and use formula to calculate channels after convolution

src/models/CNN.py
@@ -7,13 +7,15 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         """Basic Structure of a Convolutional Neural Network, 2 Convolutional Layers, 2 Max Pooling Layers, and 2 Fully Connected Layers"""
-        self.conv1 = Conv2d(28*28, 1024, kernel_size=(3,3))
+        # MNIST images are grayscale with 1 channel and size 28x28
+        self.conv1 = Conv2d(1, 32, kernel_size=(3,3))
         self.maxPool1 = MaxPool2d(kernel_size=(2,2))
-        self.conv2 = Conv2d(512, 256, kernel_size=(3,3))
+        self.conv2 = Conv2d(32, 64, kernel_size=(3,3))
         self.maxPool2 = MaxPool2d(kernel_size=(2,2))
 
-        self.fc1 = Linear(256, 256-32)
-        self.fc2 = Linear(256-32, 192)
+        # After two conv+pool layers the feature map size is 64 x 5 x 5
+        self.fc1 = Linear(64 * 5 * 5, 256)
+        self.fc2 = Linear(256, 192)
         self.fc3 = Linear(192, 10)
 
     def forward(self, x):