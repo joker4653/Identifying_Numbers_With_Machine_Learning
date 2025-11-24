0d46c45905c901ba77c209f46a7b8a30e52b7176
Joseph <josephfabrello@gmail.com>
2025-11-24 19:02:20 +1100

    added trainCNN using EntropyLoss and lr of 0.03

--- src/trainModels.py
+++ src/trainModels.py
@@ -4,7 +4,7 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 from torch import nn as nn
 from models.MLP import MLP
-#from models.CNN import CNN
+from models.CNN import CNN
 #from models.LTSM import LTSM
 
 train_data = datasets.MNIST(
@@ -55,7 +55,7 @@ def trainMLP(epoch):
 
         # Logging
         if batch_idx % 20 == 0:
-            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(loaders["train"])}'
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(loaders["train"].dataset)}' # type: ignore
                   f'({100. * batch_idx / len(loaders["train"]):.0f}%)]\tLoss: {loss.item():.6f}')\
                   
     model.eval()
@@ -75,9 +75,44 @@ def trainMLP(epoch):
           f'({100. * correct / len(loaders["test"].dataset):.0f}%)\n') # type: ignore
 
 
-def trainCNN():
+def trainCNN(epoch):
     """Function to train a Convolutional Neural Network (CNN) model."""
-    pass
+    model = CNN().to(device)
+
+    lossFunct = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=0.03)
+
+    """Defining the training process"""
+    model.train()
+    for batch_idx, (data, target) in enumerate(loaders["train"]):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad() # Prior to back propagation, we need to zero the gradients
+        output = model(data) # intial correct class prediction
+        loss = lossFunct(output, target) # Calculate the loss (how close it is to the correct solution)
+        loss.backward() # Propogate loss backwards (our incentive for the model to change weights)
+        optimizer.step()
+
+        # Logging
+        if batch_idx % 20 == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(loaders["train"].dataset)}' # type: ignore
+                  f'({100. * batch_idx / len(loaders["train"]):.0f}%)]\tLoss: {loss.item():.6f}')\
+                  
+    model.eval()
+    testLoss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in loaders["test"]:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            testLoss += lossFunct(output, target).item() # we calculate but do not propogate the loss
+            pred = output.argmax(dim=1, keepdim=True) # predicted class
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    testLoss /= len(loaders["test"].dataset) # type: ignore
+    print(f'\nTest set: Average loss: {testLoss:.4f}, Accuracy: {correct}/{len(loaders["test"].dataset)}' # type: ignore
+          f'({100. * correct / len(loaders["test"].dataset):.0f}%)\n') # type: ignore
 
 def trainLTSM():
     """Function to train a Long Short-Term Memory (LSTM) model."""
@@ -95,7 +130,8 @@ if __name__ == "__main__":
             for epoch in range(1, 11):
                 trainMLP(epoch)
         case "CNN":
-            trainCNN()
+            for epoch in range(1, 11):
+                trainCNN(epoch)
         case "LTSM":
             trainLTSM()
         case _:
